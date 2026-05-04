6852b9474a63baa5377edab3b3ab8fe2a12ea1cc
MOMO <mareimtarek2002@gmail.com>
2026-05-04 13:30:15 +0300

    Add files via upload

---
+++ aureliaHotelGuestSite/models/Service.php
@@ -0,0 +1,29 @@
+<?php
+class Service {
+    private $conn;
+    private $table = "service";
+
+    public $id;
+    public $name;
+    public $price;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    public function getServices() {
+        $query = "SELECT * FROM " . $this->table;
+        return $this->conn->query($query);
+    }
+
+    public function createService() {
+        $query = "INSERT INTO " . $this->table . " (name, price)
+                  VALUES (:name, :price)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":price", $this->price);
+        return $stmt->execute();
+    }
+}
+
+?>
